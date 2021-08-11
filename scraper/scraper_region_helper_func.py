650f37b6acc8cb3ba69d9e7bd37aba99e374f2d9
Eric <ericyeonpark@gmail.com>
2021-08-11 01:03:47 -0700

    replaced wrangle function with scrape function nomenclature to be more correct

--- scraper/scraper_region_helper_func.py
+++ scraper/scraper_region_helper_func.py
@@ -1,9 +1,33 @@
-from scraper import wrangle
 import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 import dak_links
 
+def scrape(link):
+    #Load the webpage content
+    r = requests.get(link)
+
+    #Convert to a beautiful soup object
+    soup = bs(r.content) 
+
+    #Extract column names from table
+    table = soup.select('table.stats-table')[0]
+    columns = table.find('thead').find_all('th')
+    column_names = [c.string for c in columns]
+
+    #Extract table rows
+    table_rows = table.find('tbody').find_all('tr')
+    
+    #Create dataframe
+    l = []
+    for tr in table_rows:
+        td = tr.find_all('td')
+        row = [str(tr.get_text()).strip() for tr in td]
+        l.append(row)
+
+    df = pd.DataFrame(l, columns = column_names)
+
+    return df
 
 def scrape_kor(link1 = dak_links.kr_link1, link2 = dak_links.kr_link2, 
                link3 = dak_links.kr_link3, link4 = dak_links.kr_link4, 
@@ -13,16 +37,16 @@ def scrape_kor(link1 = dak_links.kr_link1, link2 = dak_links.kr_link2,
     '''
     Create Top 500 Korean df's based off of dak_links py file
     '''
-    df_kr1 = wrangle(link1)
-    df_kr2 = wrangle(link2)
-    df_kr3 = wrangle(link3)
-    df_kr4 = wrangle(link4)
-    df_kr5 = wrangle(link5)
-    df_kr6 = wrangle(link6)
-    df_kr7 = wrangle(link7)
-    df_kr8 = wrangle(link8)
-    df_kr9 = wrangle(link9)
-    df_kr10 = wrangle(link10)
+    df_kr1 = scrape(link1)
+    df_kr2 = scrape(link2)
+    df_kr3 = scrape(link3)
+    df_kr4 = scrape(link4)
+    df_kr5 = scrape(link5)
+    df_kr6 = scrape(link6)
+    df_kr7 = scrape(link7)
+    df_kr8 = scrape(link8)
+    df_kr9 = scrape(link9)
+    df_kr10 = scrape(link10)
 
     return df_kr1, df_kr2, df_kr3, df_kr4, df_kr5, \
            df_kr6, df_kr7, df_kr8, df_kr9, df_kr10
@@ -36,16 +60,16 @@ def scrape_eu(link1 = dak_links.eu_link1, link2 = dak_links.eu_link2,
     '''
     Create Top 500 EU df's based off of dak_links py file
     '''
-    df_eu1 = wrangle(link1)
-    df_eu2 = wrangle(link2)
-    df_eu3 = wrangle(link3)
-    df_eu4 = wrangle(link4)
-    df_eu5 = wrangle(link5)
-    df_eu6 = wrangle(link6)
-    df_eu7 = wrangle(link7)
-    df_eu8 = wrangle(link8)
-    df_eu9 = wrangle(link9)
-    df_eu10 = wrangle(link10)
+    df_eu1 = scrape(link1)
+    df_eu2 = scrape(link2)
+    df_eu3 = scrape(link3)
+    df_eu4 = scrape(link4)
+    df_eu5 = scrape(link5)
+    df_eu6 = scrape(link6)
+    df_eu7 = scrape(link7)
+    df_eu8 = scrape(link8)
+    df_eu9 = scrape(link9)
+    df_eu10 = scrape(link10)
 
     return df_eu1, df_eu2, df_eu3, df_eu4, df_eu5, \
            df_eu6, df_eu7, df_eu8, df_eu9, df_eu10
@@ -59,16 +83,16 @@ def scrape_na(link1 = dak_links.na_link1, link2 = dak_links.na_link2,
     '''
     Create Top 500 NA df's based off of dak_links py file
     '''
-    df_na1 = wrangle(link1)
-    df_na2 = wrangle(link2)
-    df_na3 = wrangle(link3)
-    df_na4 = wrangle(link4)
-    df_na5 = wrangle(link5)
-    df_na6 = wrangle(link6)
-    df_na7 = wrangle(link7)
-    df_na8 = wrangle(link8)
-    df_na9 = wrangle(link9)
-    df_na10 = wrangle(link10)
+    df_na1 = scrape(link1)
+    df_na2 = scrape(link2)
+    df_na3 = scrape(link3)
+    df_na4 = scrape(link4)
+    df_na5 = scrape(link5)
+    df_na6 = scrape(link6)
+    df_na7 = scrape(link7)
+    df_na8 = scrape(link8)
+    df_na9 = scrape(link9)
+    df_na10 = scrape(link10)
 
     return df_na1, df_na2, df_na3, df_na4, df_na5, \
            df_na6, df_na7, df_na8, df_na9, df_na10
@@ -82,16 +106,16 @@ def scrape_ap(link1 = dak_links.ap_link1, link2 = dak_links.ap_link2,
     '''
     Create Top 500 Asia Pacific df's based off of dak_links py file
     '''
-    df_ap1 = wrangle(link1)
-    df_ap2 = wrangle(link2)
-    df_ap3 = wrangle(link3)
-    df_ap4 = wrangle(link4)
-    df_ap5 = wrangle(link5)
-    df_ap6 = wrangle(link6)
-    df_ap7 = wrangle(link7)
-    df_ap8 = wrangle(link8)
-    df_ap9 = wrangle(link9)
-    df_ap10 = wrangle(link10)
+    df_ap1 = scrape(link1)
+    df_ap2 = scrape(link2)
+    df_ap3 = scrape(link3)
+    df_ap4 = scrape(link4)
+    df_ap5 = scrape(link5)
+    df_ap6 = scrape(link6)
+    df_ap7 = scrape(link7)
+    df_ap8 = scrape(link8)
+    df_ap9 = scrape(link9)
+    df_ap10 = scrape(link10)
 
     return df_ap1, df_ap2, df_ap3, df_ap4, df_ap5, \
            df_ap6, df_ap7, df_ap8, df_ap9, df_ap10
@@ -105,16 +129,16 @@ def scrape_br(link1 = dak_links.br_link1, link2 = dak_links.br_link2,
     '''
     Create Top 500 Brazil df's based off of dak_links py file
     '''
-    df_br1 = wrangle(link1)
-    df_br2 = wrangle(link2)
-    df_br3 = wrangle(link3)
-    df_br4 = wrangle(link4)
-    df_br5 = wrangle(link5)
-    df_br6 = wrangle(link6)
-    df_br7 = wrangle(link7)
-    df_br8 = wrangle(link8)
-    df_br9 = wrangle(link9)
-    df_br10 = wrangle(link10)
+    df_br1 = scrape(link1)
+    df_br2 = scrape(link2)
+    df_br3 = scrape(link3)
+    df_br4 = scrape(link4)
+    df_br5 = scrape(link5)
+    df_br6 = scrape(link6)
+    df_br7 = scrape(link7)
+    df_br8 = scrape(link8)
+    df_br9 = scrape(link9)
+    df_br10 = scrape(link10)
 
     return df_br1, df_br2, df_br3, df_br4, df_br5, \
            df_br6, df_br7, df_br8, df_br9, df_br10
@@ -128,16 +152,16 @@ def scrape_la(link1 = dak_links.la_link1, link2 = dak_links.la_link2,
     '''
     Create Top 500 Latin America df's based off of dak_links py file
     '''
-    df_la1 = wrangle(link1)
-    df_la2 = wrangle(link2)
-    df_la3 = wrangle(link3)
-    df_la4 = wrangle(link4)
-    df_la5 = wrangle(link5)
-    df_la6 = wrangle(link6)
-    df_la7 = wrangle(link7)
-    df_la8 = wrangle(link8)
-    df_la9 = wrangle(link9)
-    df_la10 = wrangle(link10)
+    df_la1 = scrape(link1)
+    df_la2 = scrape(link2)
+    df_la3 = scrape(link3)
+    df_la4 = scrape(link4)
+    df_la5 = scrape(link5)
+    df_la6 = scrape(link6)
+    df_la7 = scrape(link7)
+    df_la8 = scrape(link8)
+    df_la9 = scrape(link9)
+    df_la10 = scrape(link10)
 
     return df_la1, df_la2, df_la3, df_la4, df_la5, \
            df_la6, df_la7, df_la8, df_la9, df_la10
