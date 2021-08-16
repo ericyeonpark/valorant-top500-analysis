151395fd663b904377369e0d9569722097bb436f
Eric <ericyeonpark@gmail.com>
2021-08-15 21:21:00 -0700

    added final scrape function that concats all the dfs. also moved all functions in scraper_region_helper_func to this py file for organization

--- scraper_app/scraper.py
+++ scraper_app/scraper.py
@@ -1,12 +1,39 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
+
+# import functions and code from other py files
 import dak_links
+
+
+def final_scrape():
+    '''
+    Use each individual scraping functions to scrape Valorant stats from the Top 500 players in the 6 major 
+    regions of Valorant
+    '''
+    # Scrape using scraping functions in scraper.py
+    df_NA = scrape_na()
+    df_EU = scrape_eu()
+    df_KR = scrape_kr()
+    df_BR = scrape_br()
+    df_LA = scrape_la()
+    df_AP = scrape_ap()
+
+    # concat df created from scraping functions
+    df_final = pd.concat([df_NA, 
+                          df_EU,
+                          df_KR, 
+                          df_BR, 
+                          df_LA, 
+                          df_AP])
+
+    return df_final
+
 
 def scrape(link):
     '''
     scrape Valorant stats from the Top 500 players in 
-    (Korea, EU, Asian Pacific, BR, LATAT)
+    (NA, Korea, EU, Asian Pacific, BR, LATAT)
     Data from dak.gg/valorant/leaderboard and updated as of 2021-03-22
     Data links are found in dak_links.py
     '''
@@ -36,7 +63,12 @@ def scrape(link):
     return df
 
 
-def scrape_kor(link1 = dak_links.kr_link1, link2 = dak_links.kr_link2, 
+'''
+The below functions are scraping functions for individual regions
+Korea, Europe, North America, Latin America, Brazil, Asia Pacific
+'''
+
+def scrape_kr(link1 = dak_links.kr_link1, link2 = dak_links.kr_link2, 
                link3 = dak_links.kr_link3, link4 = dak_links.kr_link4, 
                link5 = dak_links.kr_link5, link6 = dak_links.kr_link6,
                link7 = dak_links.kr_link7, link8 = dak_links.kr_link8,
@@ -45,7 +77,6 @@ def scrape_kor(link1 = dak_links.kr_link1, link2 = dak_links.kr_link2,
     Create Top 500 Korean df's based off of dak_links py file
     '''
     df_kr1 = scrape(link1)
-    print('worked')
     df_kr2 = scrape(link2)
     df_kr3 = scrape(link3)
     df_kr4 = scrape(link4)
@@ -56,8 +87,25 @@ def scrape_kor(link1 = dak_links.kr_link1, link2 = dak_links.kr_link2,
     df_kr9 = scrape(link9)
     df_kr10 = scrape(link10)
 
-    return df_kr1, df_kr2, df_kr3, df_kr4, df_kr5, \
-           df_kr6, df_kr7, df_kr8, df_kr9, df_kr10
+    #Concatenate the Top 500 Korean dataframe
+    df_KR = pd.concat([df_kr1, 
+       df_kr2,
+       df_kr3, 
+       df_kr4, 
+       df_kr5, 
+       df_kr6, 
+       df_kr7, 
+       df_kr8, 
+       df_kr9, 
+       df_kr10])
+
+    #Add Region feature dataframe
+    df_KR['Region'] = 'Korea'
+
+    #Drop Most Agent Columns
+    df_KR.drop(columns= 'Most Agents', inplace=True)
+
+    return df_KR
 
 
 def scrape_eu(link1 = dak_links.eu_link1, link2 = dak_links.eu_link2, 
@@ -79,8 +127,25 @@ def scrape_eu(link1 = dak_links.eu_link1, link2 = dak_links.eu_link2,
     df_eu9 = scrape(link9)
     df_eu10 = scrape(link10)
 
-    return df_eu1, df_eu2, df_eu3, df_eu4, df_eu5, \
-           df_eu6, df_eu7, df_eu8, df_eu9, df_eu10
+    #Concatenate the Top 500 European dataframe
+    df_EU = pd.concat([df_eu1, 
+                     df_eu2,
+                     df_eu3, 
+                     df_eu4, 
+                     df_eu5, 
+                     df_eu6, 
+                     df_eu7, 
+                     df_eu8, 
+                     df_eu9, 
+                     df_eu10])
+                     
+    #Add Region feature dataframe
+    df_EU['Region'] = 'Europe'
+
+    #Drop Most Agent Columns
+    df_EU.drop(columns= 'Most Agents', inplace=True)
+
+    return df_EU
 
 
 def scrape_na(link1 = dak_links.na_link1, link2 = dak_links.na_link2, 
@@ -102,8 +167,25 @@ def scrape_na(link1 = dak_links.na_link1, link2 = dak_links.na_link2,
     df_na9 = scrape(link9)
     df_na10 = scrape(link10)
 
-    return df_na1, df_na2, df_na3, df_na4, df_na5, \
-           df_na6, df_na7, df_na8, df_na9, df_na10
+    #Concatenate the Top 500 NA dataframe
+    df_NA = pd.concat([df_na1, 
+                   df_na2,
+                   df_na3, 
+                   df_na4, 
+                   df_na5, 
+                   df_na6, 
+                   df_na7, 
+                   df_na8, 
+                   df_na9, 
+                   df_na10])
+
+    #Add Region feature dataframe
+    df_NA['Region'] = 'North America'
+
+    #Drop Most Agent Columns
+    df_NA.drop(columns= 'Most Agents', inplace=True)
+
+    return df_NA
 
 
 def scrape_ap(link1 = dak_links.ap_link1, link2 = dak_links.ap_link2, 
@@ -125,8 +207,25 @@ def scrape_ap(link1 = dak_links.ap_link1, link2 = dak_links.ap_link2,
     df_ap9 = scrape(link9)
     df_ap10 = scrape(link10)
 
-    return df_ap1, df_ap2, df_ap3, df_ap4, df_ap5, \
-           df_ap6, df_ap7, df_ap8, df_ap9, df_ap10
+    #Concatenate the Top 500 AP dataframe
+    df_AP = pd.concat([df_ap1, 
+                   df_ap2,
+                   df_ap3, 
+                   df_ap4, 
+                   df_ap5, 
+                   df_ap6, 
+                   df_ap7, 
+                   df_ap8, 
+                   df_ap9, 
+                   df_ap10])
+
+    #Add Region feature dataframe
+    df_AP['Region'] = 'Asia Pacific'
+
+    #Drop Most Agent Columns
+    df_AP.drop(columns= 'Most Agents', inplace=True)
+
+    return df_AP
 
 
 def scrape_br(link1 = dak_links.br_link1, link2 = dak_links.br_link2, 
@@ -148,8 +247,25 @@ def scrape_br(link1 = dak_links.br_link1, link2 = dak_links.br_link2,
     df_br9 = scrape(link9)
     df_br10 = scrape(link10)
 
-    return df_br1, df_br2, df_br3, df_br4, df_br5, \
-           df_br6, df_br7, df_br8, df_br9, df_br10
+    #Concatenate the Top 500 BR dataframe
+    df_BR = pd.concat([df_br1, 
+                   df_br2,
+                   df_br3, 
+                   df_br4, 
+                   df_br5, 
+                   df_br6, 
+                   df_br7, 
+                   df_br8, 
+                   df_br9, 
+                   df_br10])
+
+    #Add Region feature dataframe
+    df_BR['Region'] = 'Brazil'
+
+    #Drop Most Agent Columns
+    df_BR.drop(columns= 'Most Agents', inplace=True)
+
+    return df_BR
 
 
 def scrape_la(link1 = dak_links.la_link1, link2 = dak_links.la_link2, 
@@ -171,5 +287,22 @@ def scrape_la(link1 = dak_links.la_link1, link2 = dak_links.la_link2,
     df_la9 = scrape(link9)
     df_la10 = scrape(link10)
 
-    return df_la1, df_la2, df_la3, df_la4, df_la5, \
-           df_la6, df_la7, df_la8, df_la9, df_la10
+    #Concatenate the Top 500 LATAM dataframe
+    df_LA = pd.concat([df_la1, 
+                   df_la2,
+                   df_la3, 
+                   df_la4, 
+                   df_la5, 
+                   df_la6, 
+                   df_la7, 
+                   df_la8, 
+                   df_la9, 
+                   df_la10])
+
+    #Add Region feature dataframe
+    df_LA['Region'] = 'Latin America'
+
+    #Drop Most Agent Columns
+    df_LA.drop(columns= 'Most Agents', inplace=True)
+
+    return df_LA
