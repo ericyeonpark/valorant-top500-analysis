aa0d37ec5877b38345e7b02d75adf7e958e1ecdd
Eric <ericyeonpark@gmail.com>
2021-08-15 22:02:59 -0700

    updated db functions to be more specific to Val top 500 functions

--- scraper_app/db.py
+++ scraper_app/db.py
@@ -7,7 +7,7 @@ import psycopg2
 
 load_dotenv()
 db_url = os.getenv('DB_URL')
-table_name = 'force_ranks'
+table_name = 'val500test'
 
 
 def db_action(sql_action: str):
@@ -35,15 +35,16 @@ def insert_data(data: List[Dict]):
     """ Insert data into the table """
     for item in data:
         db_action(f"""INSERT INTO {table_name} (
-        incident_date,
-        tweet_id,
-        user_name,
-        description,
-        force_rank,
-        status,
-        confidence,
-        tags,
-        src) 
+        id,
+        Rank,
+        Player,
+        Rating,
+        Winrate,
+        Games,
+        Average_Score,
+        Head_Shot_Pct,
+        Class,
+        Region) 
         VALUES {json.dumps(tuple(item.values()))};""")
 
 
@@ -55,21 +56,16 @@ def load_data() -> List[Tuple]:
 def initialize_ranks_table():
     """ This only needs to be run once when setting up the DB """
     db_action(f"""CREATE TABLE IF NOT EXISTS {table_name} (
-    incident_id SERIAL PRIMARY KEY NOT NULL,
-    incident_date TEXT,
-    tweet_id TEXT,
-    user_name TEXT,
-    description TEXT,
-    city TEXT,
-    state TEXT,
-    lat FLOAT,
-    long FLOAT,
-    title TEXT,
-    force_rank TEXT,
-    status TEXT,
-    confidence FLOAT,
-    tags TEXT,
-    src TEXT);""")
+    id SERIAL PRIMARY KEY NOT NULL,
+    Rank INT,
+    Player TEXT,
+    Rating FLOAT,
+    Winrate FLOAT,
+    Games INT,
+    Average_Score INT,
+    Head_Shot_Pct FLOAT,
+    Class TEXT,
+    Region TEXT,);""")
 
 
 def reset_table():
